9ac3fcfaffd44072028709b7b73b2b74144a801b
KenyahPalacios21 <kenyahpalacios4@gmail.com>
2025-04-15 14:06:54 -0500

    avance de landingpage

--- src/features/landing/home/home.jsx
+++ src/features/landing/home/home.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>    
+    <header className="d-flex align-items-center text-black py-7 min-vh-100 mt-5">
+    <div className="container">
+        <div className="row align-items-center">
+            <div className="col-12 col-md-6 text-center text-md-start">
+                <h1 className="header-tittle text-info">Bienvenido a <span className="text-primary">Caninos SABS</span></h1>
+                <p>Caninos SABS es una empresa colombiana dedicada al cuidado integral de los animales de compañía. Combinamos tecnología, amor por las mascotas y un enfoque profesional para ofrecer productos innovadores, servicios veterinarios especializados y atención personalizada. Nuestra plataforma te permite descubrir nuestros servicios, explorar productos de alta calidad y conocer todo lo que hacemos para mejorar la vida de tu mejor amigo peludo. En Caninos SABS, tu mascota es parte de nuestra familia. 🐶🐾</p>
+                
+                <div className="d-flex flex-column flex-md-row gap-3 justify-content-center justify-content-md-start mb-3">
+                    <button type="button" className="btn btn-primary">¡Conoce más!</button>
+                    <button type="button" className="btn btn-info">Inicia Sesión</button>
+                </div>
+            </div>
+
+            <div className="col-12 col-md-6 d-flex justify-content-center">
+                <img src="./img/Logo.png" className="img-fluid w-75" alt="Logo Caninos SABS"/>
+            </div>
+        </div>
+      </div>
+  </header>
+</>
   )
 }
--- src/features/landing/landing.jsx
+++ src/features/landing/landing.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 export const Landing = () => {
   return (
-    <div>landing</div>
+    <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Caninos SABS</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item"><Link className="nav-link active" to="#">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/landing/about">About Us</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="#">Products</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="#">Services</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="#">Categories</Link></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <Outlet />
+    </>
   )
 }
